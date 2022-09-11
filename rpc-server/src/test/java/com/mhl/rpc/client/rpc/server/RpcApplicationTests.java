01cfd6c0ee983c86b58a44992f9adc656e1b2cca
mhl <18845015806@163.com>
2022-09-11 19:21:58 +0800

    init

---
+++ rpc-server/src/test/java/com/mhl/rpc/client/rpc/server/RpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhl.rpc.client.rpc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
